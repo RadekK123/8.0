d7ce86654c76865194ba1cf988332a463b1a797b
Radek Knapkiewicz <radknap@gmail.com>
2016-09-30 20:32:58 +0200

    Add 8.2 files

---
+++ 8.2/js/firstjs.js
@@ -0,0 +1,8 @@
+
+var a = prompt('Podaj wartość podstawy trójkąta');
+var	h = prompt('Podaj wartość wysokości trójkąta');
+var	triangleArea = a * h / 2;
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
+
