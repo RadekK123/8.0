c4476efceb79a815a81d786b4a3bfd1673f37cb6
Radek Knapkiewicz <radknap@gmail.com>
2016-09-30 20:50:21 +0200

    Add files 8.6

---
+++ 8.6/js/firstjs.js
@@ -0,0 +1,12 @@
+
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var green = ('Zielone słonie').toUpperCase();
+var animal = 'Hieny';
+
+var text2 = text.replace('Papugi', green);
+
+console.log(text2.length);
+console.log(text2.slice(0,46));
+
+
+
